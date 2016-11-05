db1a25d6d2d772f7cd2ae33606cfed3eb3c5d480
Apollo Zhu <public-apollonian@outlook.com>
2016-11-04 20:27:13 -0400

    Chapter 3

--- Chapter3/3_3.py
+++ Chapter3/3_3.py
@@ -1,4 +1,16 @@
-def right_justify(msg):
-    print(' '*(70-len(msg))+msg)
+def print_grid(row,col):
+    separator = ("+"+" -"*4+" ")*col+"+\n"
+    single = ("|"+" "*9)*col+"|\n"
+    whole = single * 4
+    print((separator+whole)*row+separator)
 
-right_justify(input())
+def print_2x2_grid():
+    print_grid(2,2)
+
+def print_4x4_grid():
+    print_grid(4,4)
+
+if __name__ == "__main__":
+    print_2x2_grid()
+    print_4x4_grid()
+    print_grid(int(input("row: ")),int(input("col: ")))
