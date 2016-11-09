8585fb703618cd59a37bbe25b41c7f60041d1e53
Apollo Zhu <public-apollonian@outlook.com>
2016-11-09 17:57:25 -0500

    a-e
    teleport
    draw rect

--- Chapter4/letters.py
+++ Chapter4/letters.py
@@ -19,11 +19,19 @@ def incognito(t,func,*args,**kwargs):
     func(*args,**kwargs)
     t.pendown()
 
+def teleport(t,size,x,y):
+    pos = t.pos()
+    goto(t,pos[0]+distance(size,x),pos[1]+distance(size,y))
+
+def goto(t,x,y):
+    incognito(t,t.goto,(x,y))
+
 def resetting(t,func,*args,**kwargs):
-    pos = t.pos
+    pos = t.pos()
     heading = t.heading()
     func(*args,**kwargs)
-    t.setpos(pos)
+    goto(t,*pos)
+    # incognito(t,t.goto,pos)
     seth(t,heading)
 
 def fd(t,size,percentage):
@@ -64,25 +72,46 @@ def draw_turning(t,size,first,second):
     rt(t)
     fd(t,size,second)
 
+def draw_rect(t,size,first=0.5,second=1):
+    for i in range(2):
+        draw_turning(t,size,first,second)
+        rt(t)
+
 # Interface
 
-
 def draw_a(t,size):
-    # draw_square(t,size,0.25)
-    # seth(t)
-    # vertical(t,skip,t,distance(size,0.5))
-    # # incognito(t,vertical,t,fd,t,size,0.5)
-    # rt(t)
-    # fd(t,size,1)
-    # rt(t)
-    # fd(t,size,0.5)
     lt(t)
-    for i in range(2):
-        draw_turning(t,size,0.25,1)
-        rt(t)
-    incognito(t,vertical,t,fd,t,size,0.5)
+    draw_rect(t,size,first=0.25)
+    teleport(t,size,0,0.5)
+    seth(t)
     draw_turning(t,size,1,0.5)
 
+def draw_b(t,size):
+    lt(t)
+    draw_rect(t,size)
+    resetting(t,fd,t,size,1)
+    skip(t,size)
+
+def draw_c(t,size):
+    lt(t)
+    resetting(t,draw_turning,t,size,0.5,1)
+    seth(t)
+    fd(t,size,1)
+
+def draw_d(t,size):
+    lt(t)
+    draw_rect(t,size)
+    seth(t)
+    fd(t,size,1)
+    resetting(t,vertical,t,fd,t,size,1)
+
+def draw_e(t,size):
+    lt(t)
+    fd(t,size,0.25)
+    draw_rect(t,size,first=0.25)
+    teleport(t,size,0,-0.25)
+    seth(t)
+    fd(t,size,1)
 
 def skip(t,distance):
     seth(t)
